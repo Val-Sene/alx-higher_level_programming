01334d30f2c9f826dd16590fceb9962b0bbdf8f6
Val-Sene <62505251+Val-Sene@users.noreply.github.com>
2022-09-27 02:02:55 +0000

    Update 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    for c in my_string:
-        if c == 'c' or c == 'C':
-            new_string = my_string.replace(c, '')
+    length = len(my_string)
 
-    return(new_string)
+    j = 0
+
+    new_string = my_string[:]
+
+    for i in range(length):
+        if (my_string[i] == 'c' or my_string[i] == 'C'):
+            new_string = new_string[:(i - j)] + my_string[(i + 1):]
+            j += 1
+
+    return (new_string)
