d23eb42d91ef21f0cce292098e1764609d188f04
Val-Sene <62505251+Val-Sene@users.noreply.github.com>
2022-09-27 00:15:50 +0000

    main

---
+++ 0x03-python-data_structures/0-main.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+print_list = __import__('0-print_list_integer').print_list
+
+my_list = [1, 2, 3, 4, 5]
+print_list(my_list)
--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def print_list(list):
-    for x in list:
+def print_list(list=[]):
+    for x in range(1, len(list)+1):
         print("{:d}".format(x))
