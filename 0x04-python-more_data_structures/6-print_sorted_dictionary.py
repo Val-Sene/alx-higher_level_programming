ca0d6d209886a2d5b66c24593a68b8b0e0cbec9d
Val-Sene <62505251+Val-Sene@users.noreply.github.com>
2022-09-28 23:54:43 +0000

    Update 6-print_sorted_dictionary.py

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-def number_keys(a_dictionary):
+def print_sorted_dictionary(a_dictionary):
     sorted_list = list(a_dictionary)
     sorted_list.sort()
-
     for i in sorted_list:
         print("{}: {}".format(i, a_dictionary.get(i)))
