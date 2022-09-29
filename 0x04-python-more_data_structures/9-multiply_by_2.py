4127aa0a7ffc8a2f0c9fbed01a1d20dde8135b0b
Val-Sene <62505251+Val-Sene@users.noreply.github.com>
2022-09-29 00:40:02 +0000

    Update 9-multiply_by_2.py

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_dic = a_dictionary
-    list_keys = list(new_dic)
+    new_dir = a_dictionary.copy()
+    list_keys = list(new_dir.keys())
 
     for i in list_keys:
-        new_dic[i] *= 2
+        new_dir[i] *= 2
 
-    return (new_dic)
+    return (new_dir)
