9cfa8c9a26d9574c68c871b151859f6ba8797a5c
Val-Sene <62505251+Val-Sene@users.noreply.github.com>
2022-10-12 15:14:23 +0000

    1-square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+""" Class square to be instantiated """
+
+
+class Square:
+
+    """ Class Square that defines a square. """
+
+    def __init__(self, size="0"):
+        self.__size = size
