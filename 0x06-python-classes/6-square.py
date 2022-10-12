54b0f3b2e3cc4e8c25bdf2ed0ddf73f711014410
Val-Sene <62505251+Val-Sene@users.noreply.github.com>
2022-10-12 23:47:44 +0000

    Update 6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -7,13 +7,10 @@ class Square:
     """ Class Square that defines a square. """
 
     def __init__(self, size=0, position=(0, 0)):
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
-            self.__position = position
+        """ Method to initialize the square object
+        """
+        self.__size = size
+        self.__position = position
 
     def area(self):
         """ Method that returns the square are of the object
