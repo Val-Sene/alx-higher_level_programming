862aed4c5905a7c8634fd5bba330dd2d619752cb
Val-Sene <62505251+Val-Sene@users.noreply.github.com>
2022-10-24 22:28:57 +0000

    0-main

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -2,9 +2,12 @@
 def lookup(obj):
     """ Function that returns the list of available attributes
         and methods of an object
+
     Args:
         obj: instance of the class
+
     Returns:
         List of attributes
     """
+
     return dir(obj)
