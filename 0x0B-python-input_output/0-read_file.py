8da9c12845744e4e8dfcd6be04e82535c0b0dce6
val-sene <adjeivalentine11@gmail.com>
2022-10-25 15:31:12 -0700

    0-read_file updated

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
 """
   Module that contains a function to read all contents of a file
-  
 """
 
+
 def read_file(filename=""):
-  """ Function that reads contents of a text file
-    
-  Args:
-      filename: Name of the file
-  
-  Raises:
-      Exception: When file cannot open
-      
+    """ Function that reads contents of a text file
+
+    Args:
+        filename: Name of the file
+
+    Raises:
+        Exception: When file cannot open
   """
-  
-  with open("filename", "r", encoding="utf-8") as file:
-      print(file.read())
-    
+
+
+with open("filename", "r", encoding="utf-8") as file:
+    print(file.read())
