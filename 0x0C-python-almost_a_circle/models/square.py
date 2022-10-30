0166fb4d0d533b5ccd147a6f258b222ac61f2e26
Val-Sene <62505251+Val-Sene@users.noreply.github.com>
2022-10-30 18:40:07 +0000

    Update square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -11,4 +11,25 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """ Intializes the class instance """
         super().__init__(size, size, x, y, id)
+        
+    @property
+    def size(self):
+        """ Getting the size value """
+        return self.width
 
+    @size.setter
+    def size(self, value):
+        """ Setting the size value """
+        self.width = value
+        self.height = value
+        
+    def __str__(self):
+        """ Displays a formated string of the square """
+        first_part = "[Square] "
+        second_part = "({}) ".format(self.id)
+        third_part = "{}/{} - ".format(self.x, self.y)
+        fourth_part = "{}/{}".format(self.width, self.height)
+
+        return first_part+ second_part+ third_part+ fourth_part
+    
+   
